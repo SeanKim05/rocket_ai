730ee9257f6bc88bdfc0989165f65759cbba879f
SeanKim05 <dbgus20609@gmail.com>
2025-05-26 10:16:18 +0900

    Chore: Metadata 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,10 @@
 import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "운세박사",
+  description: "로켓 에이아이 과제전형 과제 프로젝트입니다.",
+};
 
 export default function RootLayout({
   children,
